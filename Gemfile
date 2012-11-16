f6fd9c5469b96a8fc4ab06a559419ca51544d45e
Aaron Kalair <aaronkalair@gmail.com>
2012-11-16 18:14:26 +0000

    Implemented the remaining methods in the Twitter class Configured guard to allow these methods to be tested

--- Gemfile
+++ Gemfile
@@ -4,9 +4,14 @@ group :development do
   gem 'rb-inotify'  # Monitors the filesystem for changes
   gem 'libnotify'   # Provides pretty notices when something happens
   gem 'guard-bundler' # So guard can auto download and install new gems when I add them
-  gem 'rb-fsevent', :require => false  # Monitor the file system on OS X
+  gem 'rb-fsevent'  # Monitor the file system on OS X
   gem 'growl' # Notifications on OS
   gem 'geokit' # For working with coordinates
+  gem 'twitter' # For working with the twitter REST API
+  gem 'tweetstream' # For working with the twitter streaming API
+  gem 'alchemy-api-rb', :require => 'alchemy_api' # For working with the alchemy API
+  gem 'calais' # For working with the open calais API
+  gem 'highscore' # Keyword extraction
 end
 
 group :test do
@@ -14,4 +19,5 @@ group :test do
   gem 'rspec' # The rspec testing suite
   gem 'guard-rspec'  # Automatically run the test suite when a file is modified
   gem 'vcr' # For testing API calls
+  gem 'webmock' # For testing API calls
 end 
--- spec/TwitterConnector_spec.rb
+++ spec/TwitterConnector_spec.rb
@@ -1,7 +1,15 @@
 require_relative 'spec_helper'
+require 'vcr'
+require 'webmock'
+require 'twitter'
 
 describe TwitterConnector do
   
+  VCR.configure do |c|
+    c.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
+    c.hook_into :webmock
+  end
+
   # Create a new instance of TwitterConnector that we can easily access
   let(:twitter) { TwitterConnector.new 52.4744895, -1.4845575, 5.2 }
   subject{ twitter }
@@ -18,37 +26,52 @@ describe TwitterConnector do
 
     context 'with valid input: 52.47, -1.48, 15' do
       subject { twitter.generateBoundingBox 52.47, -1.48, 15 }
-
-      specify('bounding box should == -1.8359771880161577,52.25314522071962,-1.1240228119838427,52.686854779280395') { subject.should == "-1.8359771880161577,52.25314522071962,-1.1240228119838427,52.686854779280395" }
-                                      
+      specify('bounding box should == -1.8359771880161577,52.25314522071962,-1.1240228119838427,52.686854779280395') { subject.should == "-1.8359771880161577,52.25314522071962,-1.1240228119838427,52.686854779280395" }                                     
     end
 
     context 'with invalid input' do
-
-      subject { twitter.generateBoundingBox 52.47, -1.48, -15 }
-      specify('bounding box should == error') { subject.should == "error" }
-
+      specify('should throw error') do
+        lambda{ twitter.generateBoundingBox 52.47, -1.48, -15 }.should raise_error "invalid co ordinates or radius"
+      end
     end
-
   end
 
-  context 'validateLocation function' do
+  context 'validLocation? function' do
 
     before { twitter.generateBoundingBox 52.47, -1.48, 15 }
 
     context 'with a location inside the bounding box' do
-      subject { twitter.validateLocation 52.47168432976221, -1.474764347076416 }
+      subject { twitter.validLocation? 52.47168432976221, -1.474764347076416 }
       specify('coordinates 52.47... and -1.47... should be inside the box') {subject.should be_true}
     end
 
     context 'with a location outside the bounding box' do
-      subject { twitter.validateLocation 51.51280224425956, -0.1242828369140625 }
+      subject { twitter.validLocation? 51.51280224425956, -0.1242828369140625 }
       specify('coordinates 51.51... and -0.12... should be outside the box') {subject.should be_false}
+    end
+  end
+
+  context 'getTweetsUntilX function' do
+
+    let(:test_case) { twitter.getAllTweetsUntilX }
+
+    context 'from coordinates 52.47, -1.48 with radius 5.2' do
+      specify ('there should be 100 results') do
+        VCR.use_cassette "getTweetsUntilX" do 
+          test_case.size.should equal 100
+        end 
+      end
+
+      specify ('the results should be of type Tweet') do
+        VCR.use_cassette "getTweetsUntilX" do 
+          test_case.each do |tweet|
+            tweet.should be_a_kind_of Twitter::Tweet 
+          end
+        end 
+      end
+
     end
 
   end
-
-
-
 
 end
